4bb700b7e30652393fb6f7d8845efd66f386e8d0
Jeet9693 <jeet9693@gmail.com>
2026-03-16 20:01:25 +0530

    Initial commit

---
+++ Chapter_09_Function/79_Type3_Function_WithoutReturnType.js
@@ -0,0 +1,7 @@
+function begger(money){
+    console.log("Thanks", money);
+    
+}
+
+let returnMeSomthing = begger(100);
+console.log("returnMeSomthing");
---
+++ Chapter_09_Function/80_Type4_Function_WithRuturn.js
@@ -0,0 +1,12 @@
+function SumOfTwoNumbers(a,b){
+
+    return a+b;
+
+}
+
+let c = SumOfTwoNumbers (4,5);
+let c2 = SumOfTwoNumbers (10,15);
+
+console.log(c);
+console.log(c2);
+
