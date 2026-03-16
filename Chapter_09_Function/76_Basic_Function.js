4571e1bb58680e5ba292bbcbc64e7a8a23fb92aa
Jeet9693 <jeet9693@gmail.com>
2026-03-16 19:47:53 +0530

    Initial commit

--- Chapter_09_Function/76_Basic_Function.js
+++ Chapter_09_Function/76_Basic_Function.js
@@ -1,3 +1,14 @@
+/* Synatax for function
+
+1. Defining the function
+function nameforfunction(){
+
+}
+
+2. Calling for the function */
+
+
+
 function greetByname(name){
     console.log("Hi there", name);
     
