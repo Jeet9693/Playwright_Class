93f9463cd84fcb7c5a0ec070ee0454e59ec7c39d
Jeet9693 <jeet9693@gmail.com>
2026-03-16 19:34:36 +0530

    Initial commit

---
+++ Chapter_09_Function/77_Functions.js
@@ -0,0 +1,13 @@
+// Rule to declare the function
+// 1. Define the function
+// 2. Calling the function
+
+// Defining the function
+function greetByName(name){
+    console.log("Hi", name);
+}
+// calling the function
+    
+ greetByName("Pramod");
+ greetByName("Sangeeta"); 
+
